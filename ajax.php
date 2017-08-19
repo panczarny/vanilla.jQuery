8e31bd47975d698e95633ef47ba14d4f18691c69
Michał Czarnota <michal.czarnota@schange.com>
2017-08-19 18:59:28 +0200

    Removed unused stuff

--- ajax.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$table = array();
-array_push($table, 1);
-array_push($table, 2);
-array_push($table, 3);
-echo json_encode($table);
-?>
--- bat-browser-sync.bat
+++
@@ -1,2 +0,0 @@
-cd D:\xampp\htdocs\vanilla.jQuery
-browser-sync start --no-online -p "localhost/vanilla.jQuery" -f "*.php, *.html, *.css, *.js"
